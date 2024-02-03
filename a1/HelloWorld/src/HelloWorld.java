b90876316cb394eb780a27774078c115c1a7c876
fiionaa <90001501+fionazhong518@users.noreply.github.com>
2024-02-02 16:35:33 -0800

    Add files via upload

---
+++ a1/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //System.out.println("Hello world!");
+        int i = -1436960;
+        System.out.printf("0x%x\n", i);
+    }
+}
